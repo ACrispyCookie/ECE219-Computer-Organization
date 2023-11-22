e2777fbb7afe16a19f6a82251c3d2fe6247f98da
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2023-11-22 09:48:22 +0200

    Added max stack size to C code

--- Lab3/exer2.c
+++ Lab3/exer2.c
@@ -1,17 +1,28 @@
+
 #include <stdio.h>
-int bytes = 0, called = 0;
+#define BPR 16
+
+int bytes = 0, called = 0, cur_stack_size = 0, max = 0;
 
 int f(int n, int m);
+void glbl_max (int num);
 
 int main(int argc, char *argv[]) {
     int res = f(3, 5);
-
+	
+	
     printf("%d\n", res);
-    printf("Reccursions:%d\n", bytes);
+    printf("Reccursions:%d\n", bytes*BPR);
     printf("Called %d times\n", called);
+    printf("Max stack size was: %d \n", max);
+    
+    return 0;
 }
 
 int f(int n, int m) {
+	
+	// print cur stack size
+	// printf("Called cur: %d\n", cur_stack_size);
 	
 	called++;
     if(m == 0)
@@ -19,8 +30,17 @@ int f(int n, int m) {
 
     if(n == 0)
         return m;
-    bytes++;
-
-    return f(n - 1, m) + f(n, m - 1);
+    
+	bytes++;
+	cur_stack_size++;
+	glbl_max(cur_stack_size);
+	int res = f(n - 1, m) + f(n, m - 1);
+	
+	cur_stack_size--;
+    
+	return res;
 }
 
+void glbl_max (int num) {
+	max = num > max ? num : max ;
+}
