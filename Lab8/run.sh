7ddfb997f3a5d3e5d0c9ae609784846bb0f03b79
Jason Nikas <nikasjason1@gmail.com>
2024-02-14 10:51:12 +0200

    Add Lab 9

--- Lab8/run.sh
+++ Lab8/run.sh
@@ -1,2 +1,2 @@
 make
-perf stat -e L1-dcache-loads -e L1-dcache-load-misses -e LLC-loads -e LLC-load-misses ./kmeans ./Input_Data/$1 ./Output_Data/$1 4
+perf stat -e L1-dcache-loads -e L1-dcache-load-misses -e LLC-loads -e LLC-load-misses ./kmeans ./Output_Data/$1 ./Output_Data/$1.out $2
